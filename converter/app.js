d58f25c44e7bb3b94a4af0a0886465e7c4dc18ed
marceloambarc <marcelo.ambarc@gmail.com>
2020-12-11 15:58:34 -0300

    CSV to HTML conversor

---
+++ converter/app.js
@@ -0,0 +1,22 @@
+var Reader = require("./Controller/Reader");
+var Processor = require("./Controller/Processor");
+var Table = require("./Controller/Table");
+var Writer = require("./Controller/Writer");
+
+var HtmlParser = require("./HtmlParser");
+
+var reader = new Reader();
+var writer = new Writer();
+
+async function main(){
+    var data = await reader.Read("./Resource/users.csv");
+    var processedData = Processor.Process(data);
+
+    var users = new Table(processedData);
+
+    var html = await HtmlParser.Parse(users);
+    
+    writer.Write(Date.now() + ".html",html);
+}
+
+main();
